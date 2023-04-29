024d0df7c838a41a18be215a9e17f1e23d02fd10
Muramori71 <dannyroh2500@gmail.com>
2023-04-29 13:33:12 -0700

    added variable into the function

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,6 +1,26 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var pmHour12 = $('#12PM');
+var pmHour1 = $("#1PM");
+var pmHour2 = $("#2PM");
+var pmHour3 = $("#3PM");
+var pmHour4 = $("#4PM");
+var pmHour5 = $("#5PM");
+
+var arrayForHour = [
+    pmHour12,
+    pmHour1,
+    pmHour2,
+    pmHour3,
+    pmHour4,
+    pmHour5
+
+
+];
+
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +40,6 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  $('#currentDay').text(rightNow);
 });
