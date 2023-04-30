6ee3a143d7adece374abd010ee609350a0b2a811
Muramori71 <dannyroh2500@gmail.com>
2023-04-30 14:33:24 -0700

    added an if statement

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -34,6 +34,9 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  if (id) {
+
+  }
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
