853815656d72f2535690d2ca1c8613df0f373180
Muramori71 <dannyroh2500@gmail.com>
2023-04-30 20:49:21 -0700

    added variable to the for loop

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -35,7 +35,8 @@ $(function () {
 
    for (i= 0; i < arrayForHour.length; i++ ) {
     arrayForHour.attr('class', 'row', 'time-block');
-    $('container-lg px-5').text(arrayForHour);
+    $('hour').text(arrayForHour);
+    var hour = arrayForHour[i];
    };
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
