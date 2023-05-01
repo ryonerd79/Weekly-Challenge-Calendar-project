8e9f742790f47e86c91b8e6c913b6e4d26a3029e
Muramori71 <dannyroh2500@gmail.com>
2023-05-01 14:02:54 -0700

    fixed bugs in index file updated script file and README

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,41 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-/*var amHour9 = $('#9AM');
-var amHour10 = $('#10AM');
-var amHour11 = $('#11AM');
-var pmHour12 = $('#12PM');
-var pmHour1 = $("#1PM");
-var pmHour2 = $("#2PM");
-var pmHour3 = $("#3PM");
-var pmHour4 = $("#4PM");
-var pmHour5 = $("#5PM");
-
-var arrayForHour = [
-  amHour9,
-  amHour10,
-  amHour11,
-  pmHour12,
-  pmHour1,
-  pmHour2,
-  pmHour3,
-  pmHour4,
-  pmHour5
-];
-
-var arrayTime = [
-  "9AM",
-  "10AM",
-  "11AM",
-  "12PM",
-  "1PM",
-  "2PM",
-  "3PM",
-  "4PM",
-  "5PM"
-
-
-]*/
 $('.time-block').each(function() {
  var blockHour = parseInt($(this).attr('id').split('-')[1]);
  var currentHour = dayjs().hour()
@@ -49,7 +11,8 @@ $('.time-block').each(function() {
 });
 
 var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
-  $('#currentDay').text(rightNow);
+$('#currentDay').text(rightNow);
+
 $('.saveBtn').click(function() {
   var value = $(this).siblings('.description').val();
   var time = $(this).parent().attr('id');
@@ -57,19 +20,13 @@ $('.saveBtn').click(function() {
 });
 
 $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
-/*$(function () {
-  
-  for (i= 0; i < arrayForHour.length; i++) {
-   arrayForHour[i].attr('class', 'container-lg px 5 row time-block col-2 col-md-1 hour py-3');
-   arrayForHour[i].text(arrayTime[i]);
-   
-   };
 
-   for (i= 0; i < arrayForHour.length; i++) {
-      $('root').addClass("row time-block");
-      textarea.text(arrayTime[i]);
-
-   }; 
-   
-});*/
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Weekly-Challenge-Calendar-project
+
+## Description
+
+This weekly challenge is on a password generator. This helps me in using the fundamentals in javaScript like variables, if statements, and for loop. 
+
+## Screenshot
+
+![Screenshot(24)](https://user-images.githubusercontent.com/95495757/232847900-ba9f4d97-54d9-4a73-b9c4-1994ea78b75e.png)
+
+## links
